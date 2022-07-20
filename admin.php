53f5e7b266619de7b1e9f10ee2f17aa3b5adf63d
Ramtop <ramtopramtop@rambler.ru>
2022-07-20 17:46:05 +0300

    реструктуризация данных и кода

--- admin.php
+++ admin.php
@@ -155,7 +155,8 @@ if($User_rights['Access_Rights']==2)
     }
   }
 
-  #обработка создания города
+  #обработка добавления города
+
   if(isset($_POST['town_create']))
   {
     try
@@ -173,6 +174,104 @@ if($User_rights['Access_Rights']==2)
     }
   }
   
+  #обработка изменения сезона
+
+  if(isset($_POST['seazon_change']))
+  {
+    try
+    {   
+      $dbh->beginTransaction();
+      $registration=$dbh->prepare("UPDATE Seazon SET Seazon.Name=:PDO_SeazonName,Seazon.Route_ID=:PDO_SeazonRouteID WHERE Seazon.ID=:PDO_SeazonID");
+      $registration->bindparam(':PDO_SeazonID',$_POST['seazon_id']);
+      $registration->bindparam(':PDO_SeazonName',$_POST['seazon_name']);
+      $registration->bindparam(':PDO_SeazonRouteID',$_POST['seazon_route']);
+      $registration->execute();
+      $dbh->commit();
+    }
+    catch (Exception $e)
+    {
+      $dbh->rollBack();
+      echo "Ошибка: " . $e->getMessage();
+    }
+  }
+
+  #обработка удаления сезона
+  
+  if(isset($_POST['seazon_delete']))
+  {
+    try
+    {   
+      $dbh->beginTransaction();
+      $registration=$dbh->prepare("DELETE FROM Seazon WHERE Seazon.ID=:PDO_SeazonID");
+      $registration->bindparam(':PDO_SeazonID',$_POST['seazon_id']);
+      $registration->execute();
+      $dbh->commit();
+    }
+    catch (Exception $e)
+    {
+      $dbh->rollBack();
+      echo "Ошибка: " . $e->getMessage();
+    }
+  }
+
+  #обработка добавления сезона
+  if(isset($_POST['seazon_create']))
+  {
+    try
+    {   
+      $dbh->beginTransaction();
+      $registration=$dbh->prepare("INSERT INTO Seazon SET Seazon.Name=:PDO_SeazonName, Seazon.Route_ID=:PDO_SeazonRouteID");
+      $registration->bindparam(':PDO_SeazonName',$_POST['seazon_name']);
+      $registration->bindparam(':PDO_SeazonRouteID',$_POST['seazon_route']);
+      $registration->execute();
+      $dbh->commit();
+    }
+    catch (Exception $e)
+    {
+      $dbh->rollBack();
+      echo "Ошибка: " . $e->getMessage();
+    }
+  }
+
+  #обработка удаления дня из списка дней сезона
+  
+  if(isset($_POST['seazon_day_delete']))
+  {
+    try
+    {   
+      $dbh->beginTransaction();
+      $registration=$dbh->prepare("DELETE FROM Seazon_days WHERE Seazon_days.ID=:PDO_SeazonDayID");
+      $registration->bindparam(':PDO_SeazonDayID',$_POST['seazon_day_id']);
+      $registration->execute();
+      $dbh->commit();
+    }
+    catch (Exception $e)
+    {
+      $dbh->rollBack();
+      echo "Ошибка: " . $e->getMessage();
+    }
+  }
+
+  #обработка добавления даты в маршрут
+
+  if(isset($_POST['seazon_day_create']))
+  {
+    try
+    {   
+      $dbh->beginTransaction();
+      $registration=$dbh->prepare("INSERT INTO Seazon_days SET Seazon_days.Day=:PDO_SeazonDayDay, Seazon_days.Seazon_ID=:PDO_SeazonDaySeazonID");
+      $registration->bindparam(':PDO_SeazonDayDay',$_POST['seazon_day_day']);
+      $registration->bindparam(':PDO_SeazonDaySeazonID',$_POST['seazon_day_seazon']);
+      $registration->execute();
+      $dbh->commit();
+    }
+    catch (Exception $e)
+    {
+      $dbh->rollBack();
+      echo "Ошибка: " . $e->getMessage();
+    }
+  }
+
   
   #приветствие для администратора
 
@@ -196,12 +295,11 @@ if($User_rights['Access_Rights']==2)
 
   foreach($Users_list as list($list_User_ID, $list_User_Name, $list_User_Login, $list_User_Access_Rights))
   {
-    echo '<form id="User'.$list_User_ID.'" method="POST" onsubmit="return add_scroll(User'.$list_User_ID.')">
+    echo '<form method="POST">
     Пользователь: '.$list_User_Name.', логин: '.$list_User_Login.', права: '.$list_User_Access_Rights.'
     <input name="user_rights_change_ID" type="hidden" value="'.$list_User_ID.'">
     <input name="user_rights_change_current" type="hidden" value="'.$list_User_Access_Rights.'">
     <input name="user_rights_change" type="submit" value="Изменить права">
-    <input type="hidden" name="scroll" value="0">
     </form>';
 
   }
@@ -216,49 +314,148 @@ if($User_rights['Access_Rights']==2)
   #Выгрузка списка городов
   foreach($Towns_list as list($list_Town_ID, $list_Town_Name))
   {
-    echo '<form id="Town'.$list_Town_ID.'" method="POST" onsubmit="return add_scroll(Town'.$list_Town_ID.')">
+    echo '<form method="POST">
     Город <input name="town_name" type="text" value="'.$list_Town_Name.'">
     <input name="town_id" type="hidden" value="'.$list_Town_ID.'">
     <input name="town_change" type="submit" value="Изменить название города">
     <input name="town_delete" type="submit" value="Удалить город">
-    <input type="hidden" name="scroll" value="0">
     </form>';
   }
-  echo '<form id="Town0" method="POST" onsubmit="return add_scroll(Town0)">
+
+  #форма добавления города
+
+  echo '<form method="POST">
   Город <input name="town_name" type="text">
   <input name="town_create" type="submit" value="Добавить город">
-  <input type="hidden" name="scroll" value="0">
   </form>';
-  echo '<h1>Ведение справочника сезонов</h1>';
 
-  #Формирование списка сезонов
+  #Формирование списка маршрутов
 
-  $query_seazons_list=$dbh->prepare("SELECT Seazon.ID, Seazon.Name, Seazon.Town_ID  FROM Seazon");
-  $query_seazons_list->execute();
-  $Seazons_list=$query_seazons_list->fetchAll();
+  echo '<h1>Ведение справочника маршрутов</h1>';
+  $query_routes_list=$dbh->prepare("SELECT Route.ID, Route.Name, Route.Town_ID  FROM Route");
+  $query_routes_list->execute();
+  $Routes_list=$query_routes_list->fetchAll();
 
-  #Выгрузка списка сезонов
-  foreach($Seazons_list as list($list_Seazon_ID,$list_Seazon_Name,$list_Seazon_Town_ID))
+  #Выгрузка списка маршрутов
+
+  foreach($Routes_list as list($list_Route_ID,$list_Route_Name,$list_Route_Town_ID))
   {
-    echo '<form id="Seazon'.$list_Seazon_ID.'" method="POST" onsubmit="return add_scroll(Town'.$list_Seazon_ID.')">
-    Сезон <input name="seazon_name" type="text" value="'.$list_Seazon_Name.'">
-    <select name="seazon_town">';
+    echo '<form method="POST">
+    Сезон <input name="route_name" type="text" value="'.$list_Route_Name.'">
+    <select name="route_town">';
     foreach($Towns_list as list($list_Town_ID, $list_Town_Name))
     {
       echo '<option value="'.$list_Town_ID.'" ';
-      if ($list_Town_ID==$list_Seazon_Town_ID)
+      if ($list_Town_ID==$list_Route_Town_ID)
       {
         echo 'selected';
       }
       echo '>'.$list_Town_Name.'</option>';
     }
     echo '</select>
+    <input name="seazon_id" type="hidden" value="'.$list_Route_ID.'">
+    <input name="seazon_change" type="submit" value="Изменить маршрут">
+    <input name="seazon_delete" type="submit" value="Удалить маршрут">
+    </form>';
+  }
+
+  #выгрузка формы добавления маршрута
+
+  echo '<form method="POST">
+  Маршрут <input name="route_name" type="text">
+  <select name="route_town" required>';
+  foreach($Towns_list as list($list_Town_ID, $list_Town_Name))
+    {
+      echo '<option value="'.$list_Town_ID.'">'.$list_Town_Name.'</option>';
+    }
+  echo '</select>
+  <input name="route_create" type="submit" value="Добавить маршрут">
+  </form>';
+
+  #Формирование списка сезонов
+
+  echo '<h1>Ведение справочника сезонов</h1>';
+  $query_seazons_list=$dbh->prepare("SELECT Seazon.ID, Seazon.Name, Seazon.Route_ID  FROM Seazon");
+  $query_seazons_list->execute();
+  $Seazons_list=$query_seazons_list->fetchAll();
+
+  #Выгрузка списка сезонов
+
+  foreach($Seazons_list as list($list_Seazon_ID,$list_Seazon_Name,$list_Seazon_Route_ID))
+  {
+    echo '<form method="POST">
+    Сезон <input name="seazon_name" type="text" value="'.$list_Seazon_Name.'">
+    <select name="seazon_route">';
+    foreach($Routes_list as list($list_Route_ID,$list_Route_Name,$list_Route_Town_ID))
+    {
+      echo '<option value="'.$list_Route_ID.'" ';
+      if ($list_Route_ID==$list_Seazon_Route_ID)
+      {
+        echo 'selected';
+      }
+      echo '>'.$list_Route_Name.'</option>';
+    }
+    echo '</select>
     <input name="seazon_id" type="hidden" value="'.$list_Seazon_ID.'">
     <input name="seazon_change" type="submit" value="Изменить сезон">
     <input name="seazon_delete" type="submit" value="Удалить сезон">
-    <input type="hidden" name="scroll" value="0">
     </form>';
   }
+
+  #выгрузка формы добавления сезона
+
+  echo '<form method="POST">
+  Сезон <input name="seazon_name" type="text">
+  <select name="seazon_route" required>';
+  foreach($Routes_list as list($list_Route_ID,$list_Route_Name,$list_Route_Town_ID))
+    {
+      echo '<option value="'.$list_Route_ID.'">'.$list_Route_Name.'</option>';
+    }
+  echo '</select>
+  <input name="seazon_create" type="submit" value="Добавить сезон">
+  </form>';
+  
+  #Формирование списка дат сезона
+
+  echo '<h1>Ведение справочника дат сезонов</h1>';
+  $query_days_list=$dbh->prepare("SELECT Seazon_days.ID, Seazon_days.Day, Seazon_days.Seazon_ID FROM Seazon_days");
+  $query_days_list->execute();
+  $Days_list=$query_days_list->fetchAll();
+
+  #Выгрузка списка дат сезона
+
+  foreach($Days_list as list($list_Seazon_day_ID,$list_Seazon_day_Day,$list_Seazon_day_Seazon_ID))
+  {
+    echo '<form method="POST">
+    Дата <input name="seazon_day_day" type="date" value="'.$list_Route_day_Day.'">
+    <select name="seazon_day_seazon">';
+    foreach($Seazons_list as list($list_Seazon_ID,$list_Seazon_Name,$list_Seazon_Route_ID))
+    {
+      echo '<option value="'.$list_Seazon_ID.'" ';
+      if ($list_Seazon_ID==$list_Seazon_day_Seazon_ID)
+      {
+        echo 'selected';
+      }
+      echo '>'.$list_Seazon_Name.'</option>';
+    }
+    echo '</select>
+    <input name="seazon_day_id" type="hidden" value="'.$list_Seazon_day_ID.'">
+    <input name="seazon_day_delete" type="submit" value="Удалить дату из сезона">
+    </form>';
+  }
+
+  #выгрузка формы добавления даты сезона
+
+  echo '<form method="POST">
+  Дата <input name="seazon_day_day" type="date">
+  <select name="seazon_day_seazon" required>';
+  foreach($Seazons_list as list($list_Seazon_ID,$list_Seazon_Name,$list_Seazon_Route_ID))
+    {
+      echo '<option value="'.$list_Seazon_ID.'">'.$list_Seazon_Name.'</option>';
+    }
+  echo '</select>
+  <input name="seazon_day_create" type="submit" value="Добавить день в сезон">
+  </form>';
 
 }
 else
