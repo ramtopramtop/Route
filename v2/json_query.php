fdb2adf5b7db1d05aa7d14ee01d684844f800c6c
ramtop <ramtopramtop@rambler.ru>
2022-10-12 15:10:30 +0300

    OOP_test

--- v2/json_query.php
+++ v2/json_query.php
@@ -1,18 +1,62 @@
 <?php //обработка json-запросов/json-query processing
-class User_registration //регистрирующийся пользователь/registering user
+//Поключение данных авторизации БД
+require '../../conn/dbase.php';
+
+class User_registering //регистрирующийся пользователь/registering user
 {
     private $Name; //имя пользователя/user name
     private $login; //логин пользователя/user login
     private $password;//пароль пользователя/user password
 
 }
-try
+
+class Compared_user implements DB
 {
-    $Name=json_decode(file_get_contents('php://input'),true);
-    echo'результат'.$Name;
+    function Record_to_base()
+    {
+        global $PDO_Host;
+        global $PDO_DB_Name;
+        global $PDO_DB_User;
+        global $PDO_DB_Pass;
+        $dbh = new PDO('mysql:host='.$PDO_Host.';dbname='.$PDO_DB_Name, $PDO_DB_User, $PDO_DB_Pass,
+            array(PDO::ATTR_PERSISTENT => true));
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        try
+                {
+                    //$hased_password = password_generation($json_data["password"]);
+                    $dbh->beginTransaction();
+                    $registration=$dbh->prepare("INSERT INTO User SET User.Login=:PDO_Login, User.Password=:PDO_Password, User.Name=:PDO_Name");
+                    $registration->bindvalue(':PDO_Login','login');
+                    $registration->bindvalue(':PDO_Password',"password");
+                    $registration->bindvalue(':PDO_Name',"name");
+                    $registration->execute();
+                    $dbh->commit();
+                }
+                catch (Exception $e)
+                {
+                    $dbh->rollBack();
+                    $e->getMessage();
+                    exit;
+                }
+    }
 }
-catch (Exception $e)
+
+interface DB
 {
-    echo $e->getMessage();
+    function Record_to_base();
 }
+
+$record=new Compared_user();
+$record->Record_to_base();
+
+//try
+//{
+//    $Name=json_decode(file_get_contents('php://input'),true);
+//    echo'результат'.$Name;
+//}
+//catch (Exception $e)
+//{
+//    echo $e->getMessage();
+//}
 ?>
