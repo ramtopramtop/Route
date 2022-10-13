abca8e09b1b764f577c315dd9da11d949a3c4acd
ramtop <ramtopramtop@rambler.ru>
2022-10-13 16:26:03 +0300

    OOP_Test

--- v2/json_query.php
+++ v2/json_query.php
@@ -2,12 +2,90 @@
 //Поключение данных авторизации БД
 require '../../conn/dbase.php';
 
-class User_registering //регистрирующийся пользователь/registering user
+class User_registering implements What_is_you_name //регистрирующийся пользователь/registering user
 {
-    private $Name; //имя пользователя/user name
+    private $name; //имя пользователя/user name
     private $login; //логин пользователя/user login
     private $password;//пароль пользователя/user password
 
+    function __construct()
+    {
+        try
+        {
+            $json_data=json_decode(file_get_contents('php://input'),true);
+            if (!isset($json_data["login"])||!isset($json_data["password"])||!isset($json_data["name"]))
+            {
+                throw new Exception('Не хватает параметров');
+            }
+            $this -> name = $json_data["name"];
+            $this -> login = $json_data["login"];
+            $this -> password = $json_data["password"];
+            echo $this -> name.' '.$this -> login.' '.$this -> password;
+        }
+        catch (Exception $e)
+        {
+            exit ($e->getMessage());
+            
+        }
+    }
+
+    function What_is_you_name()
+    {
+        return $this->name;
+    }
+}
+
+class What_is_you_name
+{
+    function What_is_you_name()
+    {
+        
+    }
+}
+
+class DB_connection
+{
+    function __construct()
+    {
+        global $PDO_Host;
+        global $PDO_DB_Name;
+        global $PDO_DB_User;
+        global $PDO_DB_Pass;
+        try
+        {
+            $this = new PDO('mysql:host='.$PDO_Host.';dbname='.$PDO_DB_Name, $PDO_DB_User, $PDO_DB_Pass,
+             array(PDO::ATTR_PERSISTENT => true));
+            $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
+    
+        }
+        catch (Exception $e)
+        {
+            exit("Не удалось подключиться: ".$e->getMessage());
+        }
+    }
+}
+
+class User_from_DB
+{
+    private $name; //имя пользователя/user name
+    private $login; //логин пользователя/user login
+    private $password;//пароль пользователя/user password
+
+    function __construct($db_connect, $searchng_user)
+    {
+        try
+        {
+            $query=$db_connect->prepare("SELECT User.ID, User.Access_Rights FROM User WHERE User.Login=:PDO_Login");
+            $query->bindparam(':PDO_Login',$searchng_user -> What_is_you_name());
+            $query->execute();
+            $Registered_user = $query->fetch(PDO::FETCH_ASSOC);  
+    
+        }
+        catch (Exception $e)
+        {
+            exit("Не удалось подключиться: ".$e->getMessage());
+        }
+    }
 }
 
 class Compared_user implements DB
@@ -36,8 +114,7 @@ class Compared_user implements DB
                 catch (Exception $e)
                 {
                     $dbh->rollBack();
-                    $e->getMessage();
-                    exit;
+                    exit($e->getMessage());
                 }
     }
 }
@@ -47,16 +124,12 @@ interface DB
     function Record_to_base();
 }
 
-$record=new Compared_user();
-$record->Record_to_base();
+//$record=new Compared_user();
+//$record->Record_to_base();
 
-//try
-//{
-//    $Name=json_decode(file_get_contents('php://input'),true);
-//    echo'результат'.$Name;
-//}
-//catch (Exception $e)
-//{
-//    echo $e->getMessage();
-//}
+$user_reg = new User_registering;
+$connection = new DB_connection;
+var_dump($user_reg, $connection);
+$user_from_db = new User_from_DB($connection, $user_reg);
+
 ?>
